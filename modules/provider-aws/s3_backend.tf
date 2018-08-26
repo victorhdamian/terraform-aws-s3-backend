02d9fb0e56cd27d184ddab326b10f7bcad56acaf
Victor H Damian Chicon <victorhdamian@gmail.com>
2018-08-26 08:43:44 -0400

    Update s3_backend.tf
    rename key reference

--- modules/provider-aws/s3_backend.tf
+++ modules/provider-aws/s3_backend.tf
@@ -22,7 +22,7 @@ resource "aws_s3_bucket" "s3_backend" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = "${aws_kms_key.vsgllc_key.arn}"
+        kms_master_key_id = "${aws_kms_key.s3_be_key.arn}"
         sse_algorithm     = "aws:kms"
       }
     }
