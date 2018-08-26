a27d2709553237265b580eec703d4f751e6b727a
Victor H Damian Chicon <victorhdamian@gmail.com>
2018-08-26 08:41:45 -0400

    Update kms_key.tf
    renamed key

--- modules/provider-aws/kms_key.tf
+++ modules/provider-aws/kms_key.tf
@@ -1,5 +1,5 @@
 #
-resource "aws_kms_key" "vsgllc_key" {
+resource "aws_kms_key" "s3_be_key" {
   description             = "This key is used to encrypt bucket objects"
   deletion_window_in_days = 10
 }
